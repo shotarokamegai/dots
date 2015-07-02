f907ec3341374e9b31397234d80dbf113ac12d1a
hagishi <hagishi.c@gmail.com>
2015-07-02 15:57:56 +0900

    add shell script
    symlink

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CURDIR=$(cd $(dirname $0); pwd)
+
+# add symlink
+ln -sf $CURDIR/.zshrc $HOME/.zshrc
+ln -sf $CURDIR/.vimrc $HOME/.vimrc
+ln -sf $CURDIR/.vimrc $HOME/.nvimrc
+ln -sf $CURDIR/.tmux.conf $HOME/.tmux.conf
+ln -sf $CURDIR/.xvimrc $HOME/.xvimrc
+
